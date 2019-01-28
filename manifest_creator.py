b762ffa1afc02e2f3782bb186f2c2037e59ac076
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-01-28 14:57:16 +0000

    Update manifest_creator.py

--- manifest_creator.py
+++ manifest_creator.py
@@ -1,10 +1,10 @@
-in_manifest = open("/home/alexander/Documents/ubuntu_manifest", 'r')
+in_manifest = open("./manifest", 'r')
 
 out_manifest_string = "sudo apt -y update && sudo apt -y upgrade && sudo apt -y full-upgrade && sudo apt -y autoremove && sudo apt -y clean && sudo apt install -y"
 
 for command in in_manifest:
     out_manifest_string += ' ' + command.split('\t')[0]
 
-out_manifest = open("/home/alexander/Documents/manifest_command", 'w')
+out_manifest = open("./manifest_command", 'w')
 
 out_manifest.write(out_manifest_string)
