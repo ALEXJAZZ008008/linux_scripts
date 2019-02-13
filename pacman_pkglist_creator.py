ca56cfe30f8b8228a6d8f159f570c7d414bc3a17
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-02-13 23:41:20 +0000

    Update pacman_pkglist_creator.py

--- pacman_pkglist_creator.py
+++ pacman_pkglist_creator.py
@@ -1,10 +1,8 @@
-in_pkglist = open("./pkglist", 'r')
+with open("./pkglist", 'r') as in_pkglist:
+    out_pkglist_string = "sudo pacman -Syyyu"
 
-out_pkglist_string = "sudo pacman -Syyyu"
+    for pkg in in_pkglist:
+        out_pkglist_string += ' ' + pkg.split('\n')[0]
 
-for command in in_pkglist:
-    out_pkglist_string += ' ' + command.split('\n')[0]
-
-out_pkglist = open("./pkglist_command", 'w')
-
-out_pkglist.write(out_pkglist_string)
+    with open("./pkglist_command", 'w') as out_pkglist:
+        out_pkglist.write(out_pkglist_string)
