96f399fac6895ce3df3a5e65d686e9345428a2ce
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-02-14 02:35:33 +0000

    Update apt_pkglist_creator.py

--- apt_pkglist_creator.py
+++ apt_pkglist_creator.py
@@ -6,7 +6,7 @@ with open("pkglist", 'r') as in_pkglist:
     for pkg in in_pkglist:
         out_pkglist_string += ' ' + pkg.split('/')[0]
 
-    out_pkglist_string += "sudo apt -y update && sudo apt -y upgrade && sudo apt -y full-upgrade && sudo do-release-upgrade && sudo apt -y update && sudo apt -y upgrade && sudo apt -y full-upgrade && sudo apt -y autoremove && sudo apt -y clean"
+    out_pkglist_string += " && sudo apt -y update && sudo apt -y upgrade && sudo apt -y full-upgrade && sudo do-release-upgrade && sudo apt -y update && sudo apt -y upgrade && sudo apt -y full-upgrade && sudo apt -y autoremove && sudo apt -y clean"
 
     with open("pkglist_command", 'w') as out_pkglist:
         out_pkglist.write(out_pkglist_string)
