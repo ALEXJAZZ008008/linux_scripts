32a092092daa4839ec8e6f50be84efca8dd2847f
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-02-14 13:26:10 +0000

    Update apt_update.sh

--- apt_update.sh
+++ apt_update.sh
@@ -1,9 +1,5 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y full-upgrade
-sudo do-release-upgrade
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y full-upgrade
 sudo apt -y autoremove
 sudo apt -y clean
