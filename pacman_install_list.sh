e063c266b2ff8c192f06c5b6411f2314a5da82dc
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-02-18 12:30:09 +0000

    Update pacman_install_list.sh

--- pacman_install_list.sh
+++ pacman_install_list.sh
@@ -1,30 +1,30 @@
-intel_gpu_drivers =       false
-nvidia_gpu_drivers =      false
-amd_gpu_drivers =         false
-virtualbox_drivers =      false
-xorg_display_manager =    true
-i3_window_manager =       false
-kde_desktop_enviroment =  false
-mate_desktop_enviroment = false
-open_source_fonts =       false
-network_drivers =         false
-networking =              true
-cmake =                   true
-git =                     true
-python =                  true
-swig =                    false
-ide =                     true
-pycharm =                 true
-w3m =                     true
-firefox_browser =         false
-chromium_browser =        true
-pdf_viewer =              true
-libre_office =            true
-vlc =                     true
-media_editors =           false
-steam =                   true
-spotify =                 true
-emulation =               true
+intel_gpu_drivers=false
+nvidia_gpu_drivers=false
+amd_gpu_drivers=false
+virtualbox_drivers=false
+xorg_display_manager=true
+i3_window_manager=false
+kde_desktop_enviroment=false
+mate_desktop_enviroment=false
+open_source_fonts=false
+network_drivers=false
+networking=true
+cmake=true
+git=true
+python=true
+swig=false
+ide=true
+pycharm=true
+w3m=true
+firefox_browser=false
+chromium_browser=true
+pdf_viewer=true
+libre_office=true
+vlc=true
+media_editors=false
+steam=true
+spotify=true
+emulation=true
 
 pacman -Syyu --noconfirm
 
@@ -49,7 +49,7 @@ for i in xf86-video-vesa xf86-video-nouveau lib32-mesa-libgl libgl mesa; do
   pacman -S --noconfirm $i
 done
 
-if["$intel_gpu_drivers" = true]; then
+if [ "$intel_gpu_drivers" = true ]; then
   #install intel gpu drivers
   for i in xf86-video-intel; do
     pacman -S --noconfirm $i
@@ -61,7 +61,7 @@ else
   done
 fi
 
-if["$nvidia_gpu_drivers" = true]; then
+if [ "$nvidia_gpu_drivers" = true ]; then
   #install nvidia gpu drivers
   for i in nvidia nvidia-lts nvidia-libgl lib32-nvidia-libgl; do
     pacman -S --noconfirm $i
@@ -73,7 +73,7 @@ else
   done
 fi
 
-if["$amd_gpu_drivers" = true]; then
+if [ "$amd_gpu_drivers" = true ]; then
   #install amd gpu drivers
   for i in xf86-video-amdgpu xf86-video-ati; do
     pacman -S --noconfirm $i
@@ -85,7 +85,7 @@ else
   done
 fi
 
-if["$virtualbox_drivers" = true]; then
+if [ "$virtualbox_drivers" = true ]; then
   #install virtualbox drivers
   for i in virtualbox-guest-utils virtualbox-guest-modules-arch; do
     pacman -S --noconfirm $i
@@ -97,7 +97,7 @@ else
   done
 fi
 
-if["$xorg_display_manager" = true]; then
+if [ "$xorg_display_manager" = true ]; then
   #install xorg display manager
   for i in xorg-server xorg-xinit xterm; do
     pacman -S --noconfirm $i
@@ -109,7 +109,7 @@ else
   done
 fi
 
-if["$i3_window_manager" = true]; then
+if [ "$i3_window_manager" = true ]; then
   #install i3 window manager
   for i in xorg-xdm xdm-archlinux i3; do
     pacman -S --noconfirm $i
@@ -127,7 +127,7 @@ else
   done
 fi
 
-if["$kde_desktop_enviroment" = true]; then
+if [ "$kde_desktop_enviroment" = true ]; then
   #install kde desktop enviroment
   for i in sddm plasma-meta plasma plasma-desktop plasma-wayland-session kde-applications kde-applications-meta; do
     pacman -S --noconfirm $i
@@ -145,7 +145,7 @@ else
   done
 fi
 
-if["$mate_desktop_enviroment" = true]; then
+if [ "$mate_desktop_enviroment" = true ]; then
   #install mate desktop enviroment
   for i in lightdm lightdm-gtk-greeter deepin-session-ui mate mate-extra mate-applet-dock mate-applet-streamer mate-menu; do
     pacman -S --noconfirm $i
@@ -163,7 +163,7 @@ else
   done
 fi
 
-if["$open_source_fonts" = true]; then
+if [ "$open_source_fonts" = true ]; then
   #install open source fonts
   for i in ttf-liberation font-bh-ttf ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid ttf-roboto noto-fonts ttf-ubuntu-font-family; do
     pacman -S --noconfirm $i
@@ -175,7 +175,7 @@ else
   done
 fi
 
-if["$network_drivers" = true]; then
+if [ "$network_drivers" = true ]; then
   #install network drivers
   for i in broadcom-wl broadcom-wl-dkms; do
     pacman -S --noconfirm $i
@@ -187,7 +187,7 @@ else
   done
 fi
 
-if["$networking" = true]; then
+if [ "$networking" = true ]; then
 #install networking
   for i in openssh dialog network-manager-applet networkmanager networkmanager-openvpn wireless_tools wpa_supplicant wpa_actiond; do
     pacman -S --noconfirm $i
@@ -205,7 +205,7 @@ else
   done
 fi
 
-if["$cmake" = true]; then
+if [ "$cmake" = true ]; then
   #install cmake
   for i in cmake extra-cmake-modules; do
     pacman -S --noconfirm $i
@@ -217,7 +217,7 @@ else
   done
 fi
 
-if["$git" = true]; then
+if [ "$git" = true ]; then
   #install git
   for i in git; do
     pacman -S --noconfirm $i
@@ -229,7 +229,7 @@ else
   done
 fi
 
-if["$python" = true]; then
+if [ "$python" = true ]; then
   #install python
   for i in python2 python pypy pypy3; do
     pacman -S --noconfirm $i
@@ -241,7 +241,7 @@ else
   done
 fi
 
-if["$swig" = true]; then
+if [ "$swig" = true ]; then
   #install swig
   for i in swig; do
     pacman -S --noconfirm $i
@@ -253,7 +253,7 @@ else
   done
 fi
 
-if["$ide" = true]; then
+if [ "$ide" = true ]; then
   #install ide
   for i in qtcreator qt5-base qt4 codeblocks doxygen doxygen-docs; do
     pacman -S --noconfirm $i
@@ -265,7 +265,7 @@ else
   done
 fi
 
-if["$pycharm" = true]; then
+if [ "$pycharm" = true ]; then
   #install pycharm
   for i in pycharm-community-edition; do
     pacman -S --noconfirm $i
@@ -277,7 +277,7 @@ else
   done
 fi
 
-if["$w3m" = true]; then
+if [ "$w3m" = true ]; then
   #install w3m browser
   for i in w3m; do
     pacman -S --noconfirm $i
@@ -289,7 +289,7 @@ else
   done
 fi
 
-if["$firefox_browser" = true]; then
+if [ "$firefox_browser" = true ]; then
   #install firefox browser
   for i in firefox firefox-developer-edition arch-firefox-search; do
     pacman -S --noconfirm $i
@@ -301,7 +301,7 @@ else
   done
 fi
 
-if["$chromium_browser" = true]; then
+if [ "$chromium_browser" = true ]; then
   #install chromium browser
   for i in chromium; do
     pacman -S --noconfirm $i
@@ -313,7 +313,7 @@ else
   done
 fi
 
-if["$pdf_viewer" = true]; then
+if [ "$pdf_viewer" = true ]; then
   #install pdf viewer
   for i in zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-pdf-poppler zathura-ps; do
     pacman -S --noconfirm $i
@@ -325,7 +325,7 @@ else
   done
 fi
 
-if["$libre_office" = true]; then
+if [ "$libre_office" = true ]; then
   #install libreoffice
   for i in libreoffice-fresh libreoffice-still libreoffice-fresh-en-gb; do
     pacman -S --noconfirm $i
@@ -337,7 +337,7 @@ else
   done
 fi
 
-if["$vlc" = true]; then
+if [ "$vlc" = true ]; then
   #install vlc
   for i in vlc; do
     pacman -S --noconfirm $i
@@ -349,7 +349,7 @@ else
   done
 fi
 
-if["$media_editors" = true]; then
+if [ "$media_editors" = true ]; then
   #install media editors
   for i in gimp audacity blender; do
     pacman -S --noconfirm $i
@@ -361,7 +361,7 @@ else
   done
 fi
 
-if["$steam" = true]; then
+if [ "$steam" = true ]; then
   #install steam
   for i in steam steam-native-runtime; do
     pacman -S --noconfirm $i
@@ -373,7 +373,7 @@ else
   done
 fi
 
-if["$spotify" = true]; then
+if [ "$spotify" = true ]; then
   #install spotify
   for i in clementine; do
     pacman -S --noconfirm $i
@@ -385,7 +385,7 @@ else
   done
 fi
 
-if["$emulation" = true]; then
+if [ "$emulation" = true ]; then
   #install emulation
   for i in virtualbox virtualbox-host-modules-arch virtualbox-host-dkms wine wine-staging wine_gecko wine-mono; do
     pacman -S --noconfirm $i
