ae02cc8bdeb6fc7020761285bf6df6bacdccc652
Alexander C Whitehead <alexandercwhitehead@gmail.com>
2019-02-25 17:33:37 +0000

    fixed dpkg and rm of cuda

--- apt_matlab.sh
+++ apt_matlab.sh
@@ -16,9 +16,9 @@ done
 
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
-dpkg -i ./cuda-repo-ubuntu1804_10.0.130–1_amd64.deb
+dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
-rm ./cuda-repo-ubuntu1804_10.0.130–1_amd64.deb
+rm ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 
