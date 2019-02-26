f5d66f3d4f2081867017022dfa2814b6a8e071f9
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-02-26 22:19:17 +0000

    Update apt_matlab.sh

--- apt_matlab.sh
+++ apt_matlab.sh
@@ -30,15 +30,12 @@ for i in update upgrade full-upgrade autoremove clean; do
   apt $i -y
 done
 
-for i in cuda cuda-9-1 cuda-command-line-tools-9-1 cuda-compiler-9-1 cuda-cublas-9-1 cuda-cublas-dev-9-1 cuda-cudart-9-1 cuda-cudart-dev-9-1 cuda-cufft-9-1 cuda-cufft-dev-9-1 cuda-cuobjdump-9-1 cuda-cupti-9-1 cuda-curand-9-1 cuda-curand-dev-9-1 cuda-cusolver-9-1 cuda-cusolver-dev-9-1 cuda-cusparse-9-1 cuda-cusparse-dev-9-1 cuda-demo-suite-9-1 cuda-documentation-9-1 cuda-driver-dev-9-1 cuda-drivers cuda-gdb-9-1 cuda-gpu-library-advisor-9-1 cuda-libraries-9-1 cuda-libraries-dev-9-1 cuda-license-9-1 cuda-memcheck-9-1 cuda-misc-headers-9-1 cuda-npp-9-1 cuda-npp-dev-9-1 cuda-nsight-9-1 cuda-nvcc-9-1 cuda-nvdisasm-9-1 cuda-nvgraph-9-1 cuda-nvgraph-dev-9-1 cuda-nvml-dev-9-1 cuda-nvprof-9-1 cuda-nvprune-9-1 cuda-nvrtc-9-1 cuda-nvrtc-dev-9-1 cuda-nvtx-9-1 cuda-nvvp-9-1 cuda-repo-ubuntu1604 cuda-runtime-9-1 cuda-samples-9-1 cuda-toolkit-9-1 cuda-tools-9-1 cuda-visual-tools-9-1 libcuda1-390 nvidia-cuda-toolkit; do
+for i in cuda; do
   apt install -y $i
 done
 
-#Use furiusisomount to mount and install Matlab ISO in /home/$user/Matlab and install matlab
+#mount and install Matlab ISO in /mnt
 #$user will be replaced by your user name. (must).
-for i in furiusisomount fuseiso; do
-  apt install -y $i
-done
 exit 0
 
 ln -s /lib/i386-linux-gnu/libc.so.6 /lib/libc.so.6
