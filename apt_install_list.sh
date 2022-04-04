3865348d58f3fd84b1dcedcd87964443d86af1d8
Alexander C. Whitehead <alexandercwhitehead@gmail.com>
2022-04-04 03:05:55 +0100

    Update apt_install_list.sh

--- apt_install_list.sh
+++ apt_install_list.sh
@@ -1065,7 +1065,7 @@ then
   echo -e "\n\nInstalling jrmomo dep...\n"
 
   #Install python for jrmomo
-  for i in python-numpy python-scipy python-matplotlib python-nibabel python3-numpy python3-scipy python3-matplotlib python3-nibabel ipython python-pandas python-sympy python-nose
+  for i in python-numpy python-scipy python-matplotlib python-nibabel python3-numpy python3-scipy python3-matplotlib python3-nibabel ipython python-pandas python-sympy python-nose libopenexr-dev openexr
   do
     apt install -y $i
   done
@@ -1157,7 +1157,7 @@ else
 
   echo -e "\n\nUnstalling amide...\n"
   
-  #Unstall medical imaging viewer compatible with hv for stir and nii for nifty*
+  #Uninstall medical imaging viewer compatible with hv for stir and nii for nifty*
   for i in amide
   do
     apt purge -y $i
@@ -1167,7 +1167,7 @@ else
 
   echo -e "\n\nUnstalling stir dependencies...\n"
 
-  #Unstall dependencies for stir
+  #Uninstall dependencies for stir
   for i in gcc g++ make  cmake-curses-gui libncurses-dev libx11-dev libboost-dev libpng++-dev libpng-dev libpng-tools libpng16-16 tcsh python swig python-dev python-numpy ipython python-matplotlib mayavi2 mpi-default-dev mpi-default-bin libinsighttoolkit4-dev libtiff5-dev
   do
     apt purge -y $i
@@ -1177,7 +1177,7 @@ else
 
   echo -e "\n\nUnstalling doxygen...\n"
 
-  #Unstall documentation generator
+  #Uninstall documentation generator
   for i in doxygen graphviz graphviz-dev libgraphviz-dev python-pygraphviz python3-graphviz python-pydot python3-pydot
   do
     apt purge -y $i
@@ -1187,7 +1187,7 @@ else
 
   echo -e "\n\nUnstalling ffmpeg...\n"
 
-  #Unstall video codec for jrmomo
+  #Uninstall video codec for jrmomo
   for i in ffmpeg
   do
     apt purge -y $i
@@ -1197,7 +1197,7 @@ else
 
   echo -e "\n\nUnstalling nifty dep...\n"
 
-  #Unstall qt for nifty*
+  #Uninstall qt for nifty*
   for i in libqt5svg5 libqt5svg5-dev libqt5webkit5 libqt5webkit5-dev libqt5xmlpatterns5 libqt5xmlpatterns5-dev qttools5-dev
   do
     apt purge -y $i
@@ -1207,8 +1207,8 @@ else
 
   echo -e "\n\nUnstalling jrmomo dep...\n"
 
-  #Unstall python for jrmomo
-  for i in python-numpy python-scipy python-matplotlib python-nibabel python3-numpy python3-scipy python3-matplotlib python3-nibabel ipython python-pandas python-sympy python-nose
+  #Uninstall python for jrmomo
+  for i in python-numpy python-scipy python-matplotlib python-nibabel python3-numpy python3-scipy python3-matplotlib python3-nibabel ipython python-pandas python-sympy python-nose libopenexr-dev openexr
   do
     apt purge -y $i
   done
@@ -1217,7 +1217,7 @@ else
 
   echo -e "\n\nUnstalling niftyreg dep...\n"
 
-  #Unstall dep for niftyreg
+  #Uninstall dep for niftyreg
   for i in ocl-icd-dev ocl-icd-libopencl1 ocl-icd-opencl-dev opencl-headers 
   do
     apt purge -y $i
@@ -1227,7 +1227,7 @@ else
 
   echo -e "\n\nUnstalling sirf dep...\n"
 
-  #Unstall dep for sirf
+  #Uninstall dep for sirf
   for i in libomp-dev cython cython3 libhdf5-dev hdf5-tools python3-hdf5storage libace-6.4.5 libace-dev libace-tkreactor-6.4.5 libace-tkreactor-dev libace-xtreactor-6.4.5 libace-xtreactor-dev libopenblas-base libopenblas-dev
   do
     apt purge -y $i
@@ -1237,7 +1237,7 @@ else
 
   echo -e "\n\nUnstalling matlab dep...\n"
 
-  #Unstall dep for matlab
+  #Uninstall dep for matlab
   for i in libgmp-dev libmpfr-dev libmpc-dev gcc-6 g++-6 g++-6-multilib gfortran-6
   do
     apt purge -y $i
@@ -1257,7 +1257,7 @@ else
   
   echo -e "\n\nUninstalling xcat dep...\n"
   
-  #Unstall xcat parallel
+  #Uninstall xcat parallel
   for i in parallel
   do
     apt purge -y $i
